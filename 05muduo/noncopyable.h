02ab78b84c5da8a0ef1526a5f8178807d2aa6bca
fengsharp <canfeng12123@126.com>
2020-04-06 19:45:06 +0800

    atomic

---
+++ 05muduo/noncopyable.h
@@ -0,0 +1,16 @@
+#ifndef MUDUO_BASE_NONCOPYABLE_H
+#define MUDUO_BASE_NONCOPYABLE_H
+
+class noncopyable
+{
+public:
+  noncopyable(const noncopyable&) = delete;
+  void operator=(const noncopyable&) = delete;
+
+protected:
+  noncopyable() = default;
+  ~noncopyable() = default;
+};
+
+
+#endif
