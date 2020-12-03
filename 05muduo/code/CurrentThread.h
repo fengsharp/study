2209ac916c1374242f303d6bcbbd9dc030cbf75e
canfeng <canfeng12123@126.com>
2020-12-04 00:06:21 +0800

    mutex

--- 05muduo/code/CurrentThread.h
+++ 05muduo/code/CurrentThread.h
@@ -10,7 +10,7 @@ extern __thread char t_tidString[32];
 extern __thread int t_tidStringLength;
 extern __thread const char * t_threadName;
 
-inline pid_t gettid();
+pid_t gettid();
 void cacheTid();
 
 inline int tid()
