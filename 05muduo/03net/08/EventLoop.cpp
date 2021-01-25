37bcd4ab837a60a468772611f2a555f1e6d9b96f
canfeng <canfeng12123@126.com>
2021-01-25 16:17:51 +0800

    update

--- 05muduo/03net/08/EventLoop.cpp
+++ 05muduo/03net/08/EventLoop.cpp
@@ -1,18 +1,14 @@
 #include "EventLoop.h"
 
 #include <assert.h>
-#include <vector>
 
 #include "CurrentThread.h"
-#include "Channel.h"
 
 __thread EventLoop * gt_pLoop = NULL;
 
 EventLoop::EventLoop()
     : m_bLooping(false)
-    , m_bQuite(false)
     , m_threadId(CurrentThread::tid())
-    , m_poller(this)
 {
     assert(gt_pLoop == NULL);
     gt_pLoop = this;
@@ -27,34 +23,14 @@ EventLoop::~EventLoop()
 void EventLoop::loop()
 {
     assertLoopInThread();
+    
     assert(m_bLooping == false);
-
     m_bLooping = true;
 
-    std::vector<Channel *> vecChannels;
+    CurrentThread::sleepUsec(1000 * 1000);
+    puts("event loop.");
 
-    while (m_bQuite == false)
-    {
-        vecChannels.clear();
-        Timestamp receivedTime = m_poller.poll(-1, &vecChannels);
-
-        for (auto & item : vecChannels)
-        {
-            item->handle(receivedTime);
-        }
-    }
-    
     m_bLooping = false;
-}
-
-void EventLoop::stop()
-{
-    m_bQuite = true;
-    
-    if (isLoopInThread() == false)
-    {
-        // wake up
-    }
 }
 
 bool EventLoop::isLoopInThread() const
@@ -64,14 +40,8 @@ bool EventLoop::isLoopInThread() const
 
 void EventLoop::assertLoopInThread() const
 {
-    if (isLoopInThread() == false)
+    if (!isLoopInThread())
     {
         abort();
     }
-}
-
-void EventLoop::update(Channel * pChannel)
-{
-    assertLoopInThread();
-    m_poller.updateChannel(pChannel);
 }
--- 05muduo/03net/08/EventLoop.h
+++ 05muduo/03net/08/EventLoop.h
@@ -4,9 +4,7 @@
 #include <pthread.h>
 
 #include "NonCopyable.h"
-#include "Poller.h"
 
-class Channel;
 class EventLoop : private NonCopyable
 {
 public:
@@ -14,18 +12,12 @@ public:
     ~EventLoop();
 
     void loop();
-    void stop();
 
     bool isLoopInThread() const;
     void assertLoopInThread() const;
-
-    void update(Channel * pChannel);
 private:
     bool m_bLooping;
-    bool m_bQuite;
     const pid_t m_threadId;
-
-    Poller m_poller;
 };
 
 
